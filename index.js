4708100a9d4cb9c759e10a3200ffd7dc46adc1ec
JJKOficial <jjkjose97@gmail.com>
2024-08-07 20:37:13 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function Entrar(){
+  let numero=Number(document.getElementById("numero"));
+  let senha=Number(do
+  alert()cument.getElementById("senha"));
+if((numero+=senha)==92707593512345) {
+  alert("correto");
+}else{
+  alert("errado")
+}
+}
+function aceitar(){
+ a=document.getElementById("aceitar")
+ a.innerHTML=background-color.#d3a57d
+}
+function aceitar1(){
+ a=document.getElementById("aceitar")
+ a.innerHTML=background-color.white
+}
+alert("a");
